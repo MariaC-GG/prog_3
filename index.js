fc58f0a1e3ea11bbb022b67df69560b248b41f64
Maria <Salvito1956@gmail.com>
2023-05-15 09:42:40 -0300

    Em manutenção

---
+++ index.js
@@ -0,0 +1,16 @@
+// let btn_btn = document.querySelector('.btn_btn')
+// // console.log(btn_btn)
+// let navbar = document.querySelector('.navbar')
+// // console.log(navbar)
+
+// btn_btn.addEventListener('click', ()=>{
+//     navbar.classList.toggle('navbar_ativo')
+// })
+let menu_ham = document.querySelector('.menu_ham')
+// console.log(btn_btn)
+let btn_btn = document.querySelector('.btn_btn')
+// console.log(menu)
+
+btn_btn.addEventListener('click', ()=>{
+    menu_ham.classList.toggle('menu_nav')
+})
