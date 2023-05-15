dca7ce9020b23bcc58a6b42e5f5b69aa0909e356
Maria <Salvito1956@gmail.com>
2023-05-15 10:16:12 -0300

    arrumar css

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// let btn_btn = document.querySelector('.btn_btn')
-// // console.log(btn_btn)
-// let navbar = document.querySelector('.navbar')
-// // console.log(navbar)
-
-// btn_btn.addEventListener('click', ()=>{
-//     navbar.classList.toggle('navbar_ativo')
-// })
-let menu_ham = document.querySelector('.menu_ham')
-// console.log(btn_btn)
 let btn_btn = document.querySelector('.btn_btn')
-// console.log(menu)
+// console.log(btn_btn)
+let navbar = document.querySelector('.navbar')
+// console.log(navbar)
 
 btn_btn.addEventListener('click', ()=>{
-    menu_ham.classList.toggle('menu_nav')
+    navbar.classList.toggle('navbar_ativo')
 })
+
+
+// let menu_ham = document.querySelector('.menu_ham')
+// console.log(btn_btn)
+// let btn_menu = document.querySelector('.btn_menu')
+// console.log(menu)
+
+// btn_menu.addEventListener('click', ()=>{
+//     btn_menu.classList.toggle('btn_btn')
+// })
